5de8fcceb602f6e60b2b91a8f0b1c27d8b204d80
Q117 <125252070+EzhikIzTumana@users.noreply.github.com>
2023-05-21 19:25:43 +0300

    Сделала выгрузку контестов с сервера на страницу

--- bettersolve-client/src/components/contests/contestsPage/ContestsPage.js
+++ bettersolve-client/src/components/contests/contestsPage/ContestsPage.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ContestsComponent from "./ContestsProps";
 
-const Contests = () =>{
-    return (
-        <div className = "mainForm">
-             <ContestsComponent contest = 'Название контеста' date ='Дата начала'/>
-             <ContestsComponent contest = 'Название контеста' date ='Дата начала'/>
-             <ContestsComponent contest = 'Название контеста' date ='Дата начала'/>
-             <ContestsComponent contest = 'Название контеста' date ='Дата начала'/>
-             <ContestsComponent contest = 'Название контеста' date ='Дата начала'/>
-        </div>
-    )
-}
+const Contests = () => {
+  const [contests, setContests] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/contests")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setContests(data.contests);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+
+  return (
+    <div className="mainForm">
+      {contests.map((contest) => (
+        <ContestsComponent
+          key={contest.id}
+          contest={contest.name}
+          date={contest.start_time}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Contests;
